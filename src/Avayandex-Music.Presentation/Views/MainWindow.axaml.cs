11bf934df9be10ea0596ebfe12e10a1fc40c8227
andude10 <indko.06@gmail.com>
2022-06-21 14:55:20 +0300

    add highlighting for current page navigation button & update constants

--- src/Avayandex-Music.Presentation/Views/MainWindow.axaml.cs
+++ src/Avayandex-Music.Presentation/Views/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avayandex_Music.Presentation.Views.Controls;
+using DynamicData;
 
 namespace Avayandex_Music.Presentation.Views;
 
@@ -12,13 +13,61 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         {
             d(this.Bind(ViewModel, vm => vm.PlayerDockViewModel,
                 view => view.FindPlayerDockView.ViewModel));
+
+            if (ViewModel != null) d(ViewModel.Router.CurrentViewModel.Subscribe(HighlightNavigationButton));
         });
         AvaloniaXamlLoader.Load(this);
     }
 
+#region Methods
+
+    private void HighlightNavigationButton(IRoutableViewModel? viewModel)
+    {
+        if (ViewModel == null) return;
+
+        var vmType = ViewModel.Router.GetCurrentViewModel()?.GetType();
+
+        if (vmType == typeof(HomeViewModel))
+        {
+            FindNavigateToHomeButton.Classes.Replace("Nav-button", "Nav-button-selected");
+        }
+        else
+        {
+            FindNavigateToHomeButton.Classes.Remove("Nav-button-selected");
+            FindNavigateToHomeButton.Classes.Add("Nav-button");
+        }
+
+        if (vmType == typeof(UserTracksViewModel))
+        {
+            FindNavigateToTracksButton.Classes.Replace("Nav-button", "Nav-button-selected");
+        }
+        else
+        {
+            FindNavigateToTracksButton.Classes.Remove("Nav-button-selected");
+            FindNavigateToTracksButton.Classes.Add("Nav-button");
+        }
+
+        if (vmType == typeof(MyMusicViewModel))
+        {
+            FindNavigateToMyMusicButton.Classes.Replace("Nav-button", "Nav-button-selected");
+        }
+        else
+        {
+            FindNavigateToMyMusicButton.Classes.Remove("Nav-button-selected");
+            FindNavigateToMyMusicButton.Classes.Add("Nav-button");
+        }
+    }
+
+#endregion
+
 #region Find Properties
 
-    public PlayerDockView FindPlayerDockView => this.FindControl<PlayerDockView>("PlayerDockView");
+    public PlayerDockView FindPlayerDockView => this.FindControl<PlayerDockView>(nameof(PlayerDockView));
+    public Button FindNavigateToHomeButton => this.FindControl<Button>(nameof(NavigateToHomeButton));
+
+    public Button FindNavigateToTracksButton => this.FindControl<Button>(nameof(NavigateToTracksButton));
+
+    public Button FindNavigateToMyMusicButton => this.FindControl<Button>(nameof(NavigateToMyMusicButton));
 
 #endregion
 }
